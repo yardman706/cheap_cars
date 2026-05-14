944425338ebdd7ac6036e9238c6c2882e795c3bc
yardman706 <yardmanzee0@protonmail.com>
2026-05-14 19:06:52 +0100

    Update views.py super fixed

--- main/views.py
+++ main/views.py
@@ -2,9 +2,6 @@ from django.shortcuts import get_object_or_404, render
 from .models import Car
 
 def home(request):
-    from django.contrib.auth.models import User
-    if not User.objects.filter(username='admin').exixts():
-        User.objects.create_superuser('max237', 'max237@gmail.com', 'Unterhaltungsmedia237???')
     return render(request, 'main/home.html')
 
 
