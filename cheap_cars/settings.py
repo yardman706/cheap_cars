8f82c92470c5564288ab8c46688c2d69b8ea2749
Yardmanzee <yardmanzee0@protonmail.com>
2026-05-15 06:47:24 +0100

    remove cloudinary

--- cheap_cars/settings.py
+++ cheap_cars/settings.py
@@ -136,11 +136,3 @@ UNFOLD = {
     "SITE_TITLE": "Cheap Cars",
     "SITE_HEADER": "Cheap Cars Admin"
 }
-
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
-}
-
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
