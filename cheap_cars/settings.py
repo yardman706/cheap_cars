173bdd230c4d2132ed25f7ac882acb042b1a38c1
yardman706 <yardmanzee0@protonmail.com>
2026-05-19 16:30:53 +0100

    Update settings.py

--- cheap_cars/settings.py
+++ cheap_cars/settings.py
@@ -16,7 +16,7 @@ load_dotenv()
 SECRET_KEY = 'django-insecure-e+q^!^q%dizcs&a%o7^fvojofcmz6-7#4l&4e9#vrkw83ol3z1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["cheapcars-production.up.railway.app", "https://cheapcars-production.up.railway.app", "127.0.0.1", "cheapcarsdeals.com"]
 CSRF_TRUSTED_ORIGINS = ["https://cheapcars-production.up.railway.app"]
