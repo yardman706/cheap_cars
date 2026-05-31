987e085bc9b2f7c6cc4fed4a4213998dc3d06975
Yardmanzee <yardmanzee0@protonmail.com>
2026-05-31 15:20:04 +0100

    profile picture problem solved

--- cheap_cars/settings.py
+++ cheap_cars/settings.py
@@ -76,9 +76,10 @@ WSGI_APPLICATION = 'cheap_cars.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
